f4c64642ac0f820760e485a9e20f04161896144c
hong <1365797108@qq.com>
2024-03-12 00:07:37 +0800

    1

--- sl-express-gateway/src/main/java/com/sl/gateway/filter/CustomerTokenGatewayFilterFactory.java
+++ sl-express-gateway/src/main/java/com/sl/gateway/filter/CustomerTokenGatewayFilterFactory.java
@@ -1,15 +1,21 @@
 package com.sl.gateway.filter;
 
+import cn.hutool.core.collection.CollUtil;
+import com.itheima.auth.factory.AuthTemplateFactory;
+import com.itheima.auth.sdk.AuthTemplate;
 import com.itheima.auth.sdk.dto.AuthUserInfoDTO;
 import com.sl.gateway.config.MyConfig;
 import com.sl.gateway.properties.JwtProperties;
 import com.sl.transport.common.constant.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * 用户端token拦截处理
@@ -23,6 +29,9 @@ public class CustomerTokenGatewayFilterFactory extends AbstractGatewayFilterFact
 
     @Resource
     private JwtProperties jwtProperties;
+
+    @Value("${role.manager}")
+    private List<Long> managerRoleIds;
 
     @Override
     public GatewayFilter apply(Object config) {
@@ -38,7 +47,13 @@ public class CustomerTokenGatewayFilterFactory extends AbstractGatewayFilterFact
     @Override
     public Boolean auth(String token, AuthUserInfoDTO authUserInfoDTO, String path) {
         //普通用户不需要校验角色
-        return true;
+        //获取AuthTemplate对象
+        AuthTemplate authTemplate = AuthTemplateFactory.get(token);
+        //查询该用户的角色
+        List<Long> roleIds = authTemplate.opsForRole().findRoleByUserId(authUserInfoDTO.getUserId()).getData();
+        //取交集，判断是否有交集即可判断出是否有权限
+        Collection<Long> intersection = CollUtil.intersection(roleIds, this.managerRoleIds);
+        return CollUtil.isNotEmpty(intersection);
     }
 
     @Override
--- sl-express-gateway/src/main/java/com/sl/gateway/filter/ManagerTokenGatewayFilterFactory.java
+++ sl-express-gateway/src/main/java/com/sl/gateway/filter/ManagerTokenGatewayFilterFactory.java
@@ -1,13 +1,19 @@
 package com.sl.gateway.filter;
 
+import cn.hutool.core.collection.CollUtil;
+import com.itheima.auth.factory.AuthTemplateFactory;
+import com.itheima.auth.sdk.AuthTemplate;
 import com.itheima.auth.sdk.dto.AuthUserInfoDTO;
 import com.itheima.auth.sdk.service.TokenCheckService;
 import com.sl.gateway.config.MyConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * 后台管理员token拦截处理
@@ -19,6 +25,10 @@ public class ManagerTokenGatewayFilterFactory extends AbstractGatewayFilterFacto
     private MyConfig myConfig;
     @Resource
     private TokenCheckService tokenCheckService;
+
+    //获取配置文件中的司机角色id
+    @Value("${role.manager}")
+    private List<Long> managerRoleIds;
 
     @Override
     public GatewayFilter apply(Object config) {
@@ -34,6 +44,12 @@ public class ManagerTokenGatewayFilterFactory extends AbstractGatewayFilterFacto
 
     @Override
     public Boolean auth(String token, AuthUserInfoDTO authUserInfoDTO, String path) {
-        return true;
+        //获取AuthTemplate对象
+        AuthTemplate authTemplate = AuthTemplateFactory.get(token);
+        //查询该用户的角色
+        List<Long> roleIds = authTemplate.opsForRole().findRoleByUserId(authUserInfoDTO.getUserId()).getData();
+        //取交集，判断是否有交集即可判断出是否有权限
+        Collection<Long> intersection = CollUtil.intersection(roleIds, this.managerRoleIds);
+        return CollUtil.isNotEmpty(intersection);
     }
 }
